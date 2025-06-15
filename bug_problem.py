9cf785118efce0314fa32cf1a166e7903181e6f1
Ayzha <72868094+ayzhasprojcts@users.noreply.github.com>
2025-06-15 19:01:13 -0400

    Create bug_problem.py

---
+++ bug_problem.py
@@ -0,0 +1,7 @@
+# Initialize the accumulator.
+total = 0
+# Get the bugs collected for each day.
+for day in range (1, 8) :
+print ('Enter the bugs collected on day', day)
+bugs = int (input () )
+total += bugsl
