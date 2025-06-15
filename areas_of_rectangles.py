a7a851534a53c654924f3a770b58965d8485b484
Ayzha <72868094+ayzhasprojcts@users.noreply.github.com>
2025-06-15 18:34:44 -0400

    Create areas_of_rectangles.py

---
+++ areas_of_rectangles.py
@@ -0,0 +1,17 @@
+# Get the dimensions of rectangle 1.
+length1 = int (input ('Enter the length of rectangle 1: '))
+widthl = Int (input ('Enter the width of rectangle 1: '))
+# Get the dimensions of rectangle 2.
+length2 = int (input ('Enter the length of rectangle 2: '))
+width2 = int (inpüt ('Enter the width bf rectangle 2: '))
+# Calculate the areas of the rectangles.
+areal = lengthl * widthl
+area2 = length2 * width2
+# Determine which has the greater area.
+if areal > area2:
+  print ('Rectangle 1 has the greater area.')
+else:
+  if area2 > areal:
+    print ('Rectangle 2 has the greater area.')
+  else:
+    print ('Both have the same area.' )
